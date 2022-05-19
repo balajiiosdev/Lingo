a75fd14d69556dabfcd780ab78f5d3b39813789b
Balaji V <Balaji.V@synchronoss.com>
2022-05-19 10:36:35 +0530

    Initial Commit

---
+++ Lingo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lingo
+//
+//  Created by Balaji V on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
